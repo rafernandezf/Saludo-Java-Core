3f91bb65357bffcb7706fceea6ce5b249e89d625
Raúl Fernández <rafernandezf@hotmail.com>
2023-03-13 00:30:00 -0400

    Saludando desde Java

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.security.PermissionCollection;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java SE");
+    }
+}
